3c48f076cb22815da6fabccb4c1cb8b00d6cbf76
Alexey Gusev <gusevalexey0>
2016-08-08 00:36:11 +0300

    Some design

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,23 +1,20 @@
-import java.util.Scanner;
 
 public class MainProgram {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Run (c)lient or (s)erver");
-        if (args[0].equals("s")) {
+        if (args[0].equals("-s")) {
             {
                 int port = Integer.parseInt(args[1]);
                 new Server(port);
             }
         }
-        else if (args[0].equals("c")){
-            while (true) {
+        else if (args[0].equals("-c")){
                 String ip = args[1];
                 int port = Integer.parseInt(args[2]);
                 new Client(ip, port);
-            }
         }
-        else
-            System.out.println("Use only 's' or 'c'!");
+        else {
+            System.out.println("MainProgram -s port(0 - 65536) for server");
+            System.out.println("MainProgram -c ip port(0-65536) for client");
+        }
     }
 }
